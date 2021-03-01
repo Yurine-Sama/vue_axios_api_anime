177fc8dcad74e81300b93774ffbb540f1fbc89cc
Yurine-Sama <Yurine2019@gmail.com>
2021-03-01 20:45:26 +0700

    Anime_Api

---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">
+    <b-card
+      :img-src="image"
+      img-alt="Card image"
+      img-top
+      class="my-1"
+      height="100px"
+    >
+      <b-card-text>
+        {{ title }}
+      </b-card-text>
+    </b-card>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: String,
+    image: String
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <b-container>
+    <b-row>
+      <b-col cols="3" v-for="anime in animelist.top" :key="anime">
+        <Card :title="anime.title" :image="anime.image_url" />
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import Card from "@/components/Card.vue";
+import axios from "axios";
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Card
+  },
+  data() {
+    return {
+      animelist: {}
+    };
+  },
+
+  mounted() {
+    axios({
+      method: "GET",
+      url: "https://jikan1.p.rapidapi.com/top/anime/1/upcoming"
+    }).then(result => {
+      this.animelist = result.data;
+    });
   }
 };
 </script>
